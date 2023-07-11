a2bc2eb5210d0b058dcabc55dd94d665a737c95e
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2023-07-11 19:45:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "1711510822").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/CrezyDoll/Nidhi")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Noobxcoders/IG_Music")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
@@ -76,18 +76,18 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/3b4a340465a75c1c40eb6.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/f8af4f05aa1fc029179ad.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://te.legra.ph/file/3b4a340465a75c1c40eb6.jpg",
+    "https://graph.org/file/f8af4f05aa1fc029179ad.jpg",
 )
 
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/3b4a340465a75c1c40eb6.jpg"
 
 GLOBAL_IMG_URL = "https://te.legra.ph/file/3b4a340465a75c1c40eb6.jpg"
 
-STATS_IMG_URL = "https://te.legra.ph/file/3b4a340465a75c1c40eb6.jpg"
+STATS_IMG_URL = "https://graph.org/file/f8af4f05aa1fc029179ad.jpg"
 
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/3b4a340465a75c1c40eb6.jpg"
 
@@ -130,9 +130,9 @@ if UPSTREAM_REPO:
 if PING_IMG_URL:
     if PING_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
-            PING_IMG_URL = "https://te.legra.ph/file/3b4a340465a75c1c40eb6.jpg"
+            PING_IMG_URL = "https://graph.org/file/f8af4f05aa1fc029179ad.jpg"
 
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://te.legra.ph/file/3b4a340465a75c1c40eb6.jpg"
+            START_IMG_URL = "https://graph.org/file/f8af4f05aa1fc029179ad.jpg"
