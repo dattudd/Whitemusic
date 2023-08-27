5465637c0d895a9a18244d56bb7da76375bb3950
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2023-08-27 22:26:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "⫷ 𝙅𝙚𝙨𝙨𝙮 𝙈𝙪𝙨𝙞𝙘 ⫸")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "💐⃝♥️𝙎𝙪𝙕𝙪𝙈𝙚 ♡ 𝙈𝙪𝙨𝙞𝙘𝄟⃟🦋")
 
 OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
 
