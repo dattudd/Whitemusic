d05c092c672ae0de445dc98a040666f239ffa330
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2023-09-23 11:22:45 +0530

    Update telegraph.py

--- AnonX/plugins/telegraph.py
+++ AnonX/plugins/telegraph.py
@@ -7,10 +7,10 @@ from AnonX import app
 def ul(_, message):
     reply = message.reply_to_message
     if reply.media:
-        i = message.reply("🌹𝐌𝐚𝐤𝐢𝐧𝐠 𝐀 𝐋𝐢𝐧𝐤 𝐎𝐟 𝐘𝐨𝐮𝐫 𝐃𝐨𝐜𝐮𝐦𝐞𝐧𝐭 𝐁𝐚𝐛𝐲....🌹")
+        i = message.reply("😎 Making A Link Of Your Document Plz....😎")
         path = reply.download()
         fk = upload_file(path)
         for x in fk:
             url = "https://telegra.ph" + x
 
-        i.edit(f'🇾ᴏᴜʀ🇹ᴇʟᴇɢʀᴀᴘʜ 👉 {url}'
+        i.edit(f'🇾ᴏᴜʀ🇹ᴇʟᴇɢʀᴀᴘʜ 👉 {url}')
