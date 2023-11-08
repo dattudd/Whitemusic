5ca47e3c9cea7de5661fccac33ef2cdceedb91d1
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2023-11-08 08:20:43 +0530

    Update start.py

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -9,11 +9,11 @@ from youtubesearchpython.__future__ import VideosSearch
 import config
 from config import BANNED_USERS, OWNER_ID, MUSIC_BOT_NAME
 from strings import get_command, get_string
-from IroX import Telegram, YouTube, app
-from IroX.misc import SUDOERS, _boot_
-from IroX.plugins.playlist import del_plist_msg
-from IroX.plugins.sudoers import sudoers_list
-from IroX.utils.database import (add_served_chat,
+from AnonX import Telegram, YouTube, app
+from AnonX.misc import SUDOERS, _boot_
+from AnonX.plugins.playlist import del_plist_msg
+from AnonX.plugins.sudoers import sudoers_list
+from AnonX.utils.database import (add_served_chat,
                                        add_served_user,
                                        get_served_chats,
                                        get_served_users,
@@ -21,9 +21,9 @@ from IroX.utils.database import (add_served_chat,
                                        get_assistant, get_lang,
                                        get_userss, is_on_off,
                                        is_served_private_chat)
-from IroX.utils.decorators.language import LanguageStart
-from IroX.utils.formatters import get_readable_time
-from IroX.utils.inline import (help_pannel, private_panel,
+from AnonX.utils.decorators.language import LanguageStart
+from AnonX.utils.formatters import get_readable_time
+from AnonX.utils.inline import (help_pannel, private_panel,
                                      start_pannel)
 
 loop = asyncio.get_running_loop()
